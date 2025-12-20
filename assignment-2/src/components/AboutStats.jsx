d5f8b62911786093fff84fb4515c4349dd8af55c
Waleed <waleedeveloper100@gmail.com>
2025-12-20 08:45:00 +0500

    assignment 2 landing page completed

--- assignment-2/src/components/AboutStats.jsx
+++ assignment-2/src/components/AboutStats.jsx
@@ -1,29 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const AboutStats = () => {
+  let stats = [
+    {
+      title: "12000+",
+      line: "Hours of play annually",
+    },
+    {
+      title: "89%",
+      line: "Player Retention Rate",
+    },
+    {
+      title: "1,200+",
+      line: "Active Members",
+    },
+    {
+      title: "125+",
+      line: "Annual Tournaments",
+    },
+  ];
   return (
-    <div className='aboutStats'>
-        <h3>A few more facts about us in numbers</h3>
-        <div className="stats">
-            <div className="stat">
-                <h4>12000+</h4>
-                <p>Hours of play annually</p>
+    <div className="aboutStats">
+      <h3>A few more facts about us in numbers</h3>
+      <div className="stats">
+        {stats.map((stat, index) => {
+          return (
+            <div key={index} className="stat">
+              <h4>{stat.title}</h4>
+              <p>{stat.line}</p>
             </div>
-            <div className="stat">
-                <h4>12000+</h4>
-                <p>Hours of play annually</p>
-            </div>
-            <div className="stat">
-                <h4>12000+</h4>
-                <p>Hours of play annually</p>
-            </div>
-            <div className="stat">
-                <h4>12000+</h4>
-                <p>Hours of play annually</p>
-            </div>
-        </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutStats
+export default AboutStats;
--- assignment-2/src/components/Services.jsx
+++ assignment-2/src/components/Services.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <div className="services">
+      <div className="left">
+        <h5>Services</h5>
+        <p>
+          Explore our full range of coaching. training, and tennis experiences.
+          From first serve to match point - we've got the right program for you.
+        </p>
+        <button>Explore More</button>
+      </div>
+      <div className="center">
+        <p className="tag">Training Programs</p>
+        <div className="bottom">
+          <h5>Program designed for all ages and abilities.</h5>
+        </div>
+      </div>
+      <div className="right">
+        <div className="card">
+          <p className="tag">Training Programs</p>
+          <div className="bottom">
+            <h5>Program designed for all ages and abilities.</h5>
+          </div>
+        </div>
+          <p className="para">
+            Step into a space built for players to grow. compete, and thrive.
+          </p>
+      </div>
+    </div>
+  );
+};
 
-export default Services
+export default Services;
